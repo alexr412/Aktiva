094406fd8a70c4d50bc413fcc0eec0a958148d18
Alex Rötz <alexander.roetz@gmail.com>
2026-02-16 13:33:23 +0000

    i use geoapify can you try this as api key: ee537d684cc640eca4fdbb21b860

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,5 +4,5 @@
 // For example:
 // NEXT_PUBLIC_GEOAPIFY_API_KEY=your_geoapify_api_key
 
-export const GEOAPIFY_API_KEY = 'a2647722239d48c78a6ac13dfc694656';
+export const GEOAPIFY_API_KEY = process.env.NEXT_PUBLIC_GEOAPIFY_API_KEY || 'a2647722239d48c78a6ac13dfc694656';
 export const GOOGLE_MAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '';
